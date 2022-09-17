b175ccb1168ad48b3861025cf39f439fcf0d6f3a
vinybergamo <vinybergamo@gmail.com>
2022-09-17 11:03:56 -0300

    Create CreateAdBanner Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import { MagnifyingGlassPlus } from "phosphor-react";
-
 import "./styles/main.css";
 
 import logoImg from "./assets/Logo.svg";
 import { GameBanner } from "./components/GameBanner";
+import { CreateAdBanner } from "./components/CreateAdBanner";
 
 function App() {
   return (
@@ -24,24 +23,7 @@ function App() {
           title="League of Legends"
         />
       </div>
-
-      <div className="pt-1 mt-8 bg-nlw-gradient self-stretch rounded-lg overflow-hidden">
-        <div className="bg-[#2a2634] px-8 py-6 flex justify-between items-center">
-          <div>
-            <strong className="text-2xl text-white font-black block">
-              Não encontrou o seu duo?
-            </strong>
-            <span className="text-zinc-400">
-              Publique um anúncio para encontrar novos players!
-            </span>
-          </div>
-
-          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
-            <MagnifyingGlassPlus size="24" />
-            Publicar anúncio
-          </button>
-        </div>
-      </div>
+      <CreateAdBanner />
     </div>
   );
 }
