cd51119d0f7bf18db9eb63a4ee5354b7138b10fa
vinybergamo <vinybergamo@gmail.com>
2022-09-17 11:35:02 -0300

    Database Connection Implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from "react";
+
 import "./styles/main.css";
 
 import logoImg from "./assets/Logo.svg";
 import { GameBanner } from "./components/GameBanner";
 import { CreateAdBanner } from "./components/CreateAdBanner";
 
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  };
+}
+
 function App() {
+  const [games, setGames] = useState<Game[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3333/games")
+      .then((response) => response.json())
+      .then((data) => {
+        setGames(data);
+      });
+  }, []);
+
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt="" />
@@ -17,11 +38,16 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <GameBanner
-          bannerUrl="/game-1.png"
-          adsCount={5}
-          title="League of Legends"
-        />
+        {games.map((game) => {
+          return (
+            <GameBanner
+              key={game.id}
+              bannerUrl={game.bannerUrl}
+              adsCount={game._count.ads}
+              title={game.title}
+            />
+          );
+        })}
       </div>
       <CreateAdBanner />
     </div>
